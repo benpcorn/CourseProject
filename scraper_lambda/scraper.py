ff0bfff75f519117bc7572142d76a66e718add87
Ben Corn <benjcorn@amazon.com>
2021-11-13 14:49:20 -0500

    Multi threading

--- scraper_lambda/scraper.py
+++ scraper_lambda/scraper.py
@@ -4,13 +4,21 @@ import math
 from numpy import random
 from time import sleep
 from bs4 import BeautifulSoup
+import concurrent.futures
+
+from requests.api import head
 
 def build_request_url(asin, page_number):
     return "https://www.amazon.com/dp/product-reviews/" + asin + "/&reviewerType=all_reviews&pageNumber=" + str(page_number)
 
 
 def get_page(request_url):
-    page = requests.get(request_url)
+    headers = requests.utils.default_headers()
+    headers.update({
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+    })
+
+    page = requests.get(request_url, headers = headers)
     if page.status_code != 200:
         return None
         logging.error("Error retrieving " + request_url + " with status code: " + page.status_code)
@@ -32,9 +40,12 @@ def get_soup_from_page(page, option = "raw"):
 
 def get_global_review_count(soup):
     review_div = soup.find(attrs={"data-hook":"cr-filter-info-review-rating-count"})
-    review_count = review_div.span
-    review_count = review_count.text.strip().split(" ")[0].replace(',', '')
-    return int(review_count)
+    if review_div != None:
+        review_count = review_div.span
+        review_count = review_count.text.strip().split(" ")[0].replace(',', '')
+        return int(review_count)
+    else:
+        logging.error("Page contains no data - user agent blocking.")
 
 def get_review_page_count(review_count, reviews_per_page):
     return math.ceil(review_count / reviews_per_page)
@@ -48,18 +59,18 @@ def get_reviews_by_asin(asin):
     review_divs = []
     reviews = []
 
-    for page in range(1, review_page_count + 1):
-        if page > 1:
-            req_url = build_request_url(asin, page)
-            page = get_page(req_url)
-            soup = get_soup_from_page(page, "raw")
-        reviews_on_page = soup.find_all(attrs={"data-hook":"review-body"})
-        review_divs += reviews_on_page
+    no_threads = 10
+    with concurrent.futures.ThreadPoolExecutor(max_workers=no_threads) as executor:
+        for page in range(1, review_page_count + 1):
+            if page > 1:
+                req_url = build_request_url(asin, page)
+                page = get_page(req_url)
+                soup = get_soup_from_page(page, "raw")
+            reviews_on_page = soup.find_all(attrs={"data-hook":"review-body"})
+            review_divs += reviews_on_page
 
-        if len(reviews_on_page) < 10:
-            break
-
-        sleep(random.uniform(2, 4))
+            if len(reviews_on_page) < 10:
+                break
 
     for div in review_divs:
         reviews.append(div.get_text(strip=True))
